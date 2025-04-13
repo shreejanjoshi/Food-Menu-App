9403f72df477762a297c2ab2e6dcb75ca6c51505
Shreejan Joshi <shreejanjoshi97@gmail.com>
2025-04-13 15:47:36 +0200

    craeted all list template page and details page

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -1,7 +1,12 @@
 from . import views
 from django.urls import path
 
+app_name = 'food'
+
 urlpatterns = [
-    path('', views.index,name='index'),
-    path('item/', views.item,name='item'),
+    # /food/
+    path('', views.index, name='index'),
+    # /food/1
+    path('<int:item_id>/', views.detail, name='detail'),
+    path('item/', views.item, name='item'),
 ]

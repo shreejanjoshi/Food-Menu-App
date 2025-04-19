929d50c0a28eaf61bb7d431163e0ab528ce23784
Shreejan Joshi <shreejanjoshi97@gmail.com>
2025-04-19 15:46:41 +0200

    created navbar, base template, images fields in models

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -9,3 +9,4 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500, default="https://www.thefuzzyduck.co.uk/wp-content/uploads/2024/05/image-coming-soon-placeholder-01-660x660.png")
